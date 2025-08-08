88f0e5e224d7810f1b7ca9744dd32070168a2527
chasays <xxd0225@gmail.com>
2025-08-09 01:05:14 +0800

    add test ui nav item

---
+++ tests/ui/navigation.test.ts
@@ -0,0 +1,9 @@
+import { test, expect } from '../../src/fixtures/user-fixture';
+// todo
+test.describe('Navigation Tests', () => {
+  test('Verify Global Navigation Menu', async ({ page, homePage }) => {
+    await homePage.navigate();
+    await homePage.verifyGlobalNavigationMenuItems();
+    // todo
+  });
+}); 
